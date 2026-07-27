19cbcfd25af2c161d0020a2137504acf2891e5fe
yoshihiro-takeishi <yoshihiro.takeishi@gmail.com>
2026-07-27 22:14:59 +0900

    v5

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shop-nav-v4';
+const CACHE_NAME = 'shop-nav-v5';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/index.html', '/api/categories'])));
 });
